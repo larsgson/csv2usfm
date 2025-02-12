42958e36265afdcf2f09f8c686b216c4dcb3df48
larsgson <4819617+larsgson@users.noreply.github.com>
2025-02-12 18:57:04 +0100

    initial parser output display

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -3,8 +3,25 @@ import { useState } from 'react'
 import Header from './Header'
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from '@mui/material/CircularProgress'
+import Typography from '@mui/material/Typography'
 import {htmlText} from '../data/ruth'
+import html2usfm from '../util/html2usfmParser'
+
+
+import Modal from '@mui/material/Modal'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 700,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
 
 export default function AppLayout() {
   // eslint-disable-next-line no-unused-vars
@@ -12,6 +29,9 @@ export default function AppLayout() {
   // const [htmlText, setHtmlText] = useState()
   const [loading, setLoading] = useState(false)
   const [htmlFileLoaded, setHtmlFileLoaded] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const handleClose = () => setModalOpen(false)
 
   const handleOpen = async () => {
     setLoading(true)
@@ -38,25 +58,17 @@ export default function AppLayout() {
     // }
     // setHtmlText(htmlText)
     setHtmlFileLoaded(true)
+    // const tempUsfm = JSON.stringify(html2usfm(htmlText))
+    const tmpText = html2usfm(htmlText)
+    setUsfmText(tmpText)
     setLoading(false)
+    setModalOpen(true)
   }
 
-  const editorProps = {
-    usfmText,
-    htmlText
-  }
- 
   const appBarAndWorkSpace = 
     <div>
-      { usfmText && (
-        <div>
-          {JSON.stringify(editorProps)}
-        </div>
-      )}
       { htmlFileLoaded && (
-        <div
-          dangerouslySetInnerHTML={{__html: htmlText}}
-        />
+        <div dangerouslySetInnerHTML={{__html: htmlText}} />
       )}
     </div>
 
@@ -69,6 +81,23 @@ export default function AppLayout() {
             onOpenClick={handleOpen}
           />)}
       </Paper>
+      <div>
+        <Modal
+          open={modalOpen}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Html parser ouput (during the testing phase)
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <span dangerouslySetInnerHTML={{__html: usfmText}} />
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
       {!loading ? appBarAndWorkSpace : (
         <Box 
           sx={{ 
---
+++ src/util/helpers.js
@@ -0,0 +1,104 @@
+import UUID from 'pure-uuid';
+import base64 from 'base-64';
+
+
+const getAttributesHtml = (props) =>
+  Object.keys(props).reduce(
+    (html, propKey) =>
+      props[propKey] !== undefined && props[propKey] !== null ? (html += ` ${propKey}="${props[propKey]}"`) : html,
+    ""
+  );
+
+const getDatasetHtml = (data) =>
+  Object.keys(data).reduce(
+    (html, dataKey) =>
+      data[dataKey] ? (html += ` data-${dataKey}="${data[dataKey]}"`) : html,
+    ""
+  );
+
+const setClassList = classList => classList && Array.isArray(classList)
+  ? ` class="${classList.join(" ")}"`
+  : ` class="${classList}"`;
+
+const setElementAttributes = ({
+  classList,
+  id,
+  attributes,
+  dataset
+}) => `${setClassList(classList)}${id && ` id="${id}"`}${getDatasetHtml(dataset)}${getAttributesHtml(attributes)}`;
+
+export const createElement = (
+  {
+    tagName = "div",
+    id = "",
+    classList = "",
+    dataset = {},
+    children = "",
+    attributes = {},
+  }
+) => `<${tagName || "div"}${setElementAttributes({classList,id,attributes,dataset})}>${children}</${tagName || "div"}>`;
+
+export const mapHtml = ({ props, htmlMap }) => {
+  const { type, subtype } = props;
+  const setDefaultClassList = (type, subtype) => [...(type ? [type] : []), ...(subtype ? [subtype.replace(":", " ")] : [])];
+
+  if (!htmlMap) return { classList: setDefaultClassList(type, subtype) };
+
+  const maps = [
+    htmlMap[type]?.[subtype],
+    htmlMap["*"]?.[subtype],
+    htmlMap[type]?.["*"],
+    htmlMap["*"]?.["*"]
+  ];
+
+  const getClassList = (classList) => classList && (Array.isArray(classList) ? classList : [classList]); 
+  const result = maps.reduce((_result, map) => {
+    const _map = map || {};
+    const { classList, tagName, id, attributes } = (typeof _map === 'function') ? _map(props) : _map;
+    _result.classList = _result.classList.concat(getClassList(classList) || []);
+    _result.attributes = {..._result.attributes, ...attributes};
+    if (!_result.tagName && tagName) _result.tagName = tagName;
+    if (!_result.id && id) _result.id = id;
+    return _result;
+  }, { classList: [], tagName: "", attributes:{}});
+  return {
+    classList: result.classList.length ? [...new Set(result.classList)] : setDefaultClassList(type, subtype),
+    tagName: result.tagName,
+    id: result.id,
+    attributes :result.attributes
+  }
+}
+
+export const handleAtts = (atts) =>
+  atts
+    ? Object.keys(atts).reduce((attsProps, key) => {
+        attsProps[`atts-${key}`] =
+          typeof atts[key] === "object" ? atts[key].join(",") : atts[key];
+        return attsProps;
+      }, {})
+    : {};
+
+export const handleSubtypeNS = (subtype) => {
+  const subtypes = subtype.split(":");
+  return subtypes.length > 1
+    ? { "subtype-ns": subtypes[0], subtype: subtypes[1] }
+    : { subtype };
+};
+
+export const generateId = () => base64.encode(new UUID(4)).substring(0, 12);
+
+export const handleNewGrafts = (newGrafts) => {
+  const newSequences = {};
+  newGrafts.forEach(graft => {
+    const sequenceId = generateId();
+    newSequences[sequenceId] = {
+      type: graft.subtype,
+      blocks: graft.content || []
+    };
+    
+    graft.target = sequenceId;
+    delete graft.content;
+    delete graft.new;
+  });
+  return newSequences;
+}
